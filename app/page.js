9b2b3ed04149e4618edae87de4f72274b2b86864
Azadeh77 <ahassani4@huskers.unl.edu>
2024-04-24 18:39:56 -0500

    changes

--- app/page.js
+++ app/page.js
@@ -2,6 +2,9 @@ import Image from "next/image";
 import styles from "./page.module.css";
 import { sql } from '@vercel/postgres';
 
+
+
+
 export default async function Home() {
   const returned = await sql `SELECT username, password,email FROM login_info;`;
   let stringedReturn = JSON.stringify(returned.rows);
