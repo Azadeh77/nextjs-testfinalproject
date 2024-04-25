77d1fec192d9eb47a8ff2cdd5f441112644548b1
Azadeh77 <ahassani4@huskers.unl.edu>
2024-04-24 19:56:59 -0500

    test2

---
+++ app/dashboard/page.tsx
@@ -0,0 +1,10 @@
+import Link from "next/link";
+
+export default function Home() {
+  return (
+    <div>
+      <h1>Dashboard</h1>
+      <link href="/dashboard">Go to dashboard</link>
+    </div>
+  );
+}
