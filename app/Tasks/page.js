7a8476e32aeff38d7693132aee40e130244a1dae
Azadeh77 <ahassani4@huskers.unl.edu>
2024-05-15 18:00:27 -0500

    checkbox added

--- app/Tasks/page.js
+++ app/Tasks/page.js
@@ -1,3 +1,4 @@
+
 import Image from "next/image";
 import styles from "../page.module.css";
 import Navigation from '../../components/navigation';
@@ -5,14 +6,20 @@ import { sql } from '@vercel/postgres';
 import Link from 'next/link';
 
 
+
+
+
+
 // Define your page component
 const YourPage = async () => {
- 
-    const returned = await sql `SELECT title, description, category,	priority,	due_date,	status,	created_at FROM tasks where user_id=${"2"};`;
+  
+ //my previous code for retrieving
+    const returned = await sql `SELECT task_id, title, description, category,	priority,	due_date,	status,	created_at FROM tasks where user_id=${"2"};`;
     let stringedReturn = JSON.stringify(returned.rows);
     let dataArray = returned.rows;
     console.log(returned.rows);
-  
+
+    
   
     return (
       <div className="parent-container">
@@ -23,6 +30,8 @@ const YourPage = async () => {
             <Link href="/Tasks/NewTask">
             <button className={`${styles.myButton}`}>New Task</button>
           </Link>
+
+          <button className={`${styles.myButton}`}>Delete Task</button>
           <br></br>
           <br></br>
           <p>Current Tasks</p>
@@ -30,15 +39,28 @@ const YourPage = async () => {
        <table>
       <thead>
         <tr>
-          {dataArray.length > 0 && Object.keys(dataArray[0]).map((key) => (
-            <th key={key}>{key}</th>
-          ))}
+        <th className={styles.checkboxColumn}></th> {/* Add this line for the checkbox column header */}
+    {Object.keys(dataArray[0]).map((key, index) => {
+      // Skip the task_id
+      if (key === 'task_id') {
+        return null;
+      }
+      // Capitalize the first letter
+      key = key.charAt(0).toUpperCase() + key.slice(1);
+      return <th key={index}>{key}</th>
+    })}
         </tr>
       </thead>
       <tbody>
         {dataArray.map((item) => (
            <tr key={item.id} className={`${styles.tablerowSpacing} ${item.status === 'InProgress' ? styles.inProgressBackgroundColor : ''} ${new Date(item.due_date) < new Date() ? styles.pastDueDate : ''}`}>
+            <td className={styles.checkboxColumn}><input type="checkbox"  />
+</td>
             {Object.entries(item).map(([key, value], index) => {
+              // Skip the task_id
+        if (key === 'task_id') {
+          return null;
+        }
       // Check if value is a date
       if (value instanceof Date) {
         // Format the date to a readable string
--- pages/api/createTask.js
+++ pages/api/createTask.js
@@ -5,12 +5,12 @@ export default async function handler(req, res) {
     console.log("createTask.called");
   if (req.method === 'POST') {
     const { title, description, category, priority, dueDate, status } = req.body;
-console.log("title",title);
+console.log("description",description);
 // Convert priority to number
 const priorityNumber = priority === 'high' ? 2 : priority === 'medium' ? 1 : 0;
     try {
       await sql`INSERT INTO tasks (title, description, category, priority, due_date, status, user_id) VALUES (${title}, ${description}, ${category}, ${priorityNumber}, ${dueDate}, ${status}, 2)`;
-
+      console.log(`title: ${title}, description: ${description}, category: ${category}, priority: ${priorityNumber}, due_date: ${dueDate}, status: ${status}`);
       res.status(200).json({ message: 'Task created successfully' });
     } catch (err) {
       console.log(err);
@@ -21,3 +21,5 @@ const priorityNumber = priority === 'high' ? 2 : priority === 'medium' ? 1 : 0;
     res.status(405).json({ message: 'Method not allowed' });
   }
 }
+
+export const dynamic = 'force-dynamic';
