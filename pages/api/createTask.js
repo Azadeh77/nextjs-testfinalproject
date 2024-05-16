faca30e093aeb516916b43dd5ecd17c5cdc88922
Azadeh77 <ahassani4@huskers.unl.edu>
2024-05-15 19:22:41 -0500

    test db

--- pages/api/createTask.js
+++ pages/api/createTask.js
@@ -9,7 +9,7 @@ console.log("description",description);
 // Convert priority to number
 const priorityNumber = priority === 'high' ? 2 : priority === 'medium' ? 1 : 0;
     try {
-      await sql`INSERT INTO tasks (title, description, category, priority, due_date, status, user_id) VALUES (${title}, ${description}, ${category}, ${priorityNumber}, ${dueDate}, ${status}, 2)`;
+      await sql`INSERT INTO tasks (title, description, category, priority, due_date, status, user_id) VALUES (${title}, ${description}, ${category}, ${priorityNumber}, ${dueDate}, ${status}, "2")`;
       console.log(`title: ${title}, description: ${description}, category: ${category}, priority: ${priorityNumber}, due_date: ${dueDate}, status: ${status}`);
       res.status(200).json({ message: 'Task created successfully' });
     } catch (err) {
